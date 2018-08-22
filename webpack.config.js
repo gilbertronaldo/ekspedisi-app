84e09aeb99c3d14fae1554ff0f6ec962c5b36608
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2018-08-22 11:23:03 +0700

    frontend: typescript init

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,25 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './resources/assets/js/app.js',
+    devtool: "inline-source-map",
+    entry: './resources/assets/ts/app.ts',
+    // entry: './resources/assets/js/app.js',
     output: {
         path: path.resolve(__dirname, 'public/js'),
         filename: 'app.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            }
+        ]
     }
 };
