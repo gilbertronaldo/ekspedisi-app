a8ee693d1f8a9f2ee721c5d9fc45ee81635264de
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2018-08-25 22:50:04 +0700

    ms_ship migration & model

---
+++ app/MsShip.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilbertronaldo
+ * Date: 8/25/18
+ * Time: 10:43 PM
+ */
+
+namespace App;
+
+
+use Carbon\Carbon;
+
+/**
+ * Class MsShip
+ *
+ * @property int ship_id
+ * @property string no_voyage
+ * @property string ship_name
+ * @property string ship_description
+ * @property Carbon sailing_date
+ * @property int city_id_from
+ * @property int city_id_to
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ *
+ * @package App
+ */
+class MsShip extends BaseModel
+{
+    protected $table = 'ms_ship';
+    protected $primaryKey = 'ship_id';
+}
