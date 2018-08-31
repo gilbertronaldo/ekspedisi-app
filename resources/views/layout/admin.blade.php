78d80e96c1ed940fa600aec8984bf0a1ceebc9bc
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2018-08-31 19:30:02 +0700

    update nav menu list

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -87,11 +87,39 @@
                 <ul id="sidebarnav">
 
                     <li class="sidebar-item" ui-sref-active='selected'>
-                        <a class="sidebar-link waves-effect waves-dark sidebar-link" ui-sref="home"
+                        <a class="sidebar-link active waves-effect waves-dark sidebar-link" ui-sref="home"
                            aria-expanded="false">
                             <i class="mdi mdi-border-top"></i>
                             <span class="hide-menu">Home</span>
                         </a>
+                    </li>
+
+                    <li class="sidebar-item" ui-sref-active='selected'>
+                        <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)"
+                           aria-expanded="false">
+                            <i class="icon-Car-Wheel"></i>
+                            <span class="hide-menu">Master </span>
+                        </a>
+                        <ul aria-expanded="false" class="collapse  first-level">
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" ui-sref="ship">
+                                    <i class="icon-Record"></i>
+                                    <span class="hide-menu">Kapal</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#/recipient/view-recipient-list" class="sidebar-link">
+                                    <i class="icon-Record"></i>
+                                    <span class="hide-menu">Penerima</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#/ship/view-ship-listl" class="sidebar-link">
+                                    <i class="icon-Record"></i>
+                                    <span class="hide-menu">Pengirim</span>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </nav>
