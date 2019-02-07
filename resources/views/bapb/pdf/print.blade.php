86094db7a40bf49aa0ad00b7cb16362885acdce9
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-02-07 23:30:17 +0700

    change title pdf

--- resources/views/bapb/pdf/print.blade.php
+++ resources/views/bapb/pdf/print.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BAPB</title>
     <style>
         @page {
             margin: 150px 25px 250px;
