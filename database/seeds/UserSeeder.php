6ef3b1d18553fbbee4145ad3840604ce3bf5f12c
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-02-09 12:31:23 +0700

    seed user, role, userRoles

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
@@ -13,36 +14,71 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Truncate data
-        DB::table('users')->truncate();
+        DB::statement("TRUNCATE TABLE t_user_role RESTART IDENTITY CASCADE;");
+        DB::statement("TRUNCATE TABLE t_role RESTART IDENTITY CASCADE;");
+        DB::statement("TRUNCATE TABLE users RESTART IDENTITY CASCADE;");
 
 
-        DB::table('users')->insert([
-            'name' => 'Gilbert Ronaldo',
-            'email' => 'superadmin',
-            'password' => bcrypt('superadmin'),
-            'created_at' => \Carbon\Carbon::now()
-        ]);
+        $this->seedUser();
+        $this->seedRole();
+        $this->seedUserRole();
+    }
 
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin',
-            'password' => bcrypt('password'),
-            'created_at' => \Carbon\Carbon::now()
-        ]);
+    private function seedUser()
+    {
+        DB::table('users')->insert(
+          [
+            'name'       => 'Gilbert Ronaldo',
+            'email'      => 'superadmin',
+            'password'   => bcrypt('superadmin'),
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
+
+        DB::table('users')->insert(
+          [
+            'name'       => 'admin',
+            'email'      => 'admin',
+            'password'   => bcrypt('password'),
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
     }
 
     private function seedRole()
     {
+        DB::table('t_role')->insert(
+          [
+            'role_name'  => 'SUPERADMIN',
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
 
+        DB::table('t_role')->insert(
+          [
+            'role_name'  => 'ADMIN',
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
+
+        DB::table('t_role')->insert(
+          [
+            'role_name'  => 'STAFF',
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
     }
 
-    private function seedTask()
+
+    private function seedUserRole()
     {
-
-    }
-
-    private function seedRoleTask()
-    {
-
+        DB::table('t_user_role')->insert(
+          [
+            'user_id'    => 2,
+            'role_id'    => 2,
+            'city_code'  => 'JKT',
+            'created_at' => \Carbon\Carbon::now(),
+          ]
+        );
     }
 }
