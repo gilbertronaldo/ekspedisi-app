bd3ed6e4f4f47274d4ccf20b6fc1bd559c4e07c8
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-02-09 12:45:54 +0700

    update t_task

--- database/migrations/2019_02_09_053147_create_table_t_task.php
+++ database/migrations/2019_02_09_053147_create_table_t_task.php
@@ -22,8 +22,7 @@ class CreateTableTTask extends Migration
           function (Blueprint $table)
           {
               $table->bigIncrements('task_id');
-              $table->string('task_code', 50);
-              $table->string('task_name', 100);
+              $table->string('task_code', 255);
               $table->string('task_description', 255)->nullable();
               $table->timestamps();
               $table->softDeletes();
