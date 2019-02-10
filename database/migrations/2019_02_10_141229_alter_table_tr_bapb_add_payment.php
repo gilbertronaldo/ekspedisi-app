4b1bcb73c586bf50affcac590ad76b0e06e3dd32
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-02-10 21:15:12 +0700

    bapb migration add payment

---
+++ database/migrations/2019_02_10_141229_alter_table_tr_bapb_add_payment.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableTrBapbAddPayment extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->boolean('is_paid')->default(false);
+              $table->decimal('payment_total')->nullable();
+              $table->date('payment_date')->nullable();
+          }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->dropColumn('is_paid');
+              $table->dropColumn('payment_total');
+              $table->dropColumn('payment_date');
+          }
+        );
+    }
+}
