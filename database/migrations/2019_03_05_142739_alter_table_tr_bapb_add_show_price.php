66b50f28c9edc2088d0f84cfd1a6c1d4481595aa
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-03-05 21:29:45 +0700

    migration tr_bapb add show_price

---
+++ database/migrations/2019_03_05_142739_alter_table_tr_bapb_add_show_price.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableTrBapbAddShowPrice extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->boolean('show_price')->default(true);
+          }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->dropColumn('show_price');
+          }
+        );
+    }
+}
