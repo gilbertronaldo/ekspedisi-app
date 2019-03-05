60064f2c1cda725f98f5695ca6292e11b7480d43
Gilbert Ronaldo <gilbert.ronaldo@binus.edu>
2019-03-05 21:40:32 +0700

    fix excel

--- app/Exports/BapbExport.php
+++ app/Exports/BapbExport.php
@@ -77,12 +77,13 @@ class BapbExport implements FromView, WithEvents
             SELECT TO_CHAR(C.entry_date, 'dd FMMon') as date,
                    B.recipient_name,
                    D.sender_name,
-                   E.koli, E.bapb_sender_item_name,
+                   SUM(E.koli) AS koli, 
+                   STRING_AGG(E.bapb_sender_item_name, ', ') AS bapb_sender_item_name,
                    C.kemasan,
                    C.description,
                    C.price,
-                   (E.panjang * E.lebar * E.tinggi) AS dimensi,
-                   E.berat AS berat
+                   SUM((E.panjang * E.lebar * E.tinggi)) AS dimensi,
+                   SUM(E.berat) AS berat
             FROM tr_bapb A
             INNER JOIN ms_recipient B
               ON A.recipient_id = B.recipient_id
@@ -98,9 +99,8 @@ class BapbExport implements FromView, WithEvents
               AND E.deleted_at IS NULL
             WHERE A.deleted_at IS NULL
             AND UPPER(CONCAT(A.no_container_1, A.no_container_2)) ILIKE '$noContainer'
-            GROUP BY C.entry_date, B.recipient_name, D.sender_name, E.koli, 
-                     E.bapb_sender_item_name, C.kemasan, C.description, C.price,
-                     E.panjang, E.lebar, E.tinggi, E.berat
+            GROUP BY C.entry_date, B.recipient_name, D.sender_name, 
+                     C.kemasan, C.description, C.price
         ");
         return $get;
     }
