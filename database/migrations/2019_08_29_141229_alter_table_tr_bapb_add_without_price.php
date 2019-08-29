dd48c49f8aca91eb0c006f2b21118ffdc534eecd
Gilbert Ronaldo <gilbert@ronaldo.id>
2019-08-29 20:47:35 +0700

    migration bapt tanpa harga

---
+++ database/migrations/2019_08_29_141229_alter_table_tr_bapb_add_without_price.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableTrBapbAddPayment extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->boolean('without_price')->default(false);
+          }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+          'tr_bapb',
+          function (Blueprint $table)
+          {
+              $table->dropColumn('without_price');
+          }
+        );
+    }
+}
