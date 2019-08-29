6ce7b49c6345439788a0dbea8b5f877992c411f7
Gilbert Ronaldo <gilbert@ronaldo.id>
2019-08-29 20:48:18 +0700

    without price

--- database/migrations/2019_08_29_141229_alter_table_tr_bapb_add_without_price.php
+++ database/migrations/2019_08_29_141229_alter_table_tr_bapb_add_without_price.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableTrBapbAddPayment extends Migration
+class AlterTableTrBapbAddWithoutPrice extends Migration
 {
 
     /**
