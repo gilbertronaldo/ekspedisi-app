5593ae6e82a5cc3faf29d97b6712da00c49826fe
Gilbert Ronaldo <gilbert@ronaldo.id>
2019-09-11 18:42:24 +0700

    hide header

--- resources/views/bapb/pdf/header.blade.php
+++ resources/views/bapb/pdf/header.blade.php
@@ -1,17 +1,17 @@
 <header>
-    <div style="float: left;margin-left: 50px;">
-        <img src="{{ public_path('img/logo.png') }}" alt="" width="100" height="100">
-    </div>
-    <div>
-        <h4 style="margin: 2px;padding: 0;">EKSPEDISI</h4>
-        <h2 style="margin: 2px;padding: 0;">SUMBER REJEKI</h2>
-        <h4 style="margin: 2px;padding: 0;">Jasa Angukatan Container System</h4>
-        <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62 Jakarta
-            Utara</h5>
-        <h5 style="margin: 2px;padding: 0;">Telp. 021-6122087, 0858 7595 9469, Fax.: 021-6240380</h5>
-    </div>
-    <div style="text-align: center;width: 100%;">
-        <h2>BERITA ACARA PENYERAHAN BARANG</h2>
-    </div>
+{{--    <div style="float: left;margin-left: 50px;">--}}
+{{--        <img src="{{ public_path('img/logo.png') }}" alt="" width="100" height="100">--}}
+{{--    </div>--}}
+{{--    <div>--}}
+{{--        <h4 style="margin: 2px;padding: 0;">EKSPEDISI</h4>--}}
+{{--        <h2 style="margin: 2px;padding: 0;">SUMBER REJEKI</h2>--}}
+{{--        <h4 style="margin: 2px;padding: 0;">Jasa Angukatan Container System</h4>--}}
+{{--        <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62 Jakarta--}}
+{{--            Utara</h5>--}}
+{{--        <h5 style="margin: 2px;padding: 0;">Telp. 021-6122087, 0858 7595 9469, Fax.: 021-6240380</h5>--}}
+{{--    </div>--}}
+{{--    <div style="text-align: center;width: 100%;">--}}
+{{--        <h2>BERITA ACARA PENYERAHAN BARANG</h2>--}}
+{{--    </div>--}}
 
 </header>
