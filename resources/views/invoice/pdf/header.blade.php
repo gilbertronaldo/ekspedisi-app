ce8b8a774e84e4f5c589a2b44bce8c9553c6e4a9
Gilbert Ronaldo <gilbert@ronaldo.id>
2019-09-11 20:44:30 +0700

    header invoice

--- resources/views/invoice/pdf/header.blade.php
+++ resources/views/invoice/pdf/header.blade.php
@@ -1,19 +1,19 @@
 <header>
-{{--    <div style="float: left;margin-left: 50px;">--}}
-{{--        <img src="{{ public_path('img/logo.png') }}" alt="" width="100" height="100">--}}
-{{--    </div>--}}
-{{--    <div>--}}
-{{--        <h4 style="margin: 2px;padding: 0;">EKSPEDISI</h4>--}}
-{{--        <h2 style="margin: 2px;padding: 0;">SUMBER REJEKI</h2>--}}
-{{--        <h4 style="margin: 2px;padding: 0;">Jasa Angukatan Container System</h4>--}}
-{{--        <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62 Jakarta--}}
-{{--            Utara</h5>--}}
-{{--        <h5 style="margin: 2px;padding: 0;">Telp. 021-6122087, 0858 7595 9469, Fax.: 021-6240380</h5>--}}
-{{--    </div>--}}
-{{--    <div style="text-align: center;width: 100%;">--}}
-{{--        <h2>INVOICE</h2>--}}
-{{--    </div>--}}
-{{--    <div style="width: 100%;font-size: 16px;margin: -30px 5px 5px;text-align: left">--}}
-{{--        <span>No.</span>&nbsp;<span class="t-b">{{ $invoice->invoice_no }}</span>--}}
-{{--    </div>--}}
+    <div style="float: left;margin-left: 50px;">
+        <img src="{{ public_path('img/logo.png') }}" alt="" width="100" height="100">
+    </div>
+    <div>
+        <h4 style="margin: 2px;padding: 0;">EKSPEDISI</h4>
+        <h2 style="margin: 2px;padding: 0;">SUMBER REJEKI</h2>
+        <h4 style="margin: 2px;padding: 0;">Jasa Angukatan Container System</h4>
+        <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62 Jakarta
+            Utara</h5>
+        <h5 style="margin: 2px;padding: 0;">Telp. 021-6122087, 0858 7595 9469, Fax.: 021-6240380</h5>
+    </div>
+    <div style="text-align: center;width: 100%;">
+        <h2>INVOICE</h2>
+    </div>
+    <div style="width: 100%;font-size: 16px;margin: -30px 5px 5px;text-align: left">
+        <span>No.</span>&nbsp;<span class="t-b">{{ $invoice->invoice_no }}</span>
+    </div>
 </header>
