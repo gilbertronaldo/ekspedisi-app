aa9adb6edb67775c975ef3211a64c651ae1ed8cc
Gilbert Ronaldo <gilbert@ronaldo.id>
2021-02-27 19:09:31 +0700

    set margin invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -314,8 +314,13 @@ class InvoiceController extends Controller
 
             DB::commit();
 
-            return view('invoice.pdf.print', $input);
-            $pdf = PDF::loadView('invoice.pdf.print', $input);
+//            return view('invoice.pdf.print', $input);
+            $pdf = PDF::loadView('invoice.pdf.print', $input)
+                ->setPaper('a4')
+                ->setOptions([
+                    'margin-left'   => 10,
+                    'margin-right'  => 10,
+                ]);
 
             return $pdf->stream('invoice.pdf');
 
