4db28a354e5bbcae5e429c4681124eba76090e6e
Gilbert Ronaldo <gilbert@ronaldo.id>
2021-03-30 18:35:52 +0700

    fix bapb logo header

--- resources/views/bapb/pdf/header.blade.php
+++ resources/views/bapb/pdf/header.blade.php
@@ -1,6 +1,10 @@
 <header>
     <div style="float: left;margin-left: 50px;">
-        <img src="{{ public_path('img/logo-sr.png') }}" alt="" width="100" height="100">
+        @if($tipe === 'sr')
+            <img src="{{ public_path('img/logo-sr.png') }}" alt="" width="100" height="100">
+        @else
+            <img src="{{ public_path('img/logo-srsm.png') }}" alt="" width="100" height="100">
+        @endif
     </div>
     <div>
         @if($tipe === 'sr')
@@ -18,7 +22,8 @@
                 <h4 style="margin: 2px;padding: 0;">EKSPEDISI</h4>
                 <h2 style="margin: 2px;padding: 0;">PT. SUMBER REJEKI SINAR MANDIRI</h2>
                 <h4 style="margin: 2px;padding: 0;">Jasa Angukatan Container System</h4>
-                <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62 Jakarta
+                <h5 style="margin: 2px;padding: 0;">Jl. Mangga Dua Raya, Ruko Grand Boutique Centre Blok D No. 62
+                    Jakarta
                     Utara</h5>
                 <h5 style="margin: 2px;padding: 0;">Telp. 021-6122087, 0858 7595 9469, Fax.: 021-6240380</h5>
             </div>
